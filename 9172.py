ac6bd25755f5b03fae3e2eeba8cecd9e867056cf
Love radio v616 <45826030+loveradio1337@users.noreply.github.com>
2019-01-04 22:30:27 +0800

    Add files via upload

--- 9172.py
+++ 9172.py
@@ -3929,7 +3929,7 @@ async def on_member_join(member):
 @bot.event
 async def on_member_join(member):
     for channel in member.server.channels:
-        if channel.name == '🌟-welcome-goodbye-🌟':
+        if channel.name == '🌟-welcome-🌟':
            embed = discord.Embed(color=0xC72323)
            embed.set_author(name="🎉 New member has joined 🎉")
            embed.description = f'**Welcome ``{member.name}#{member.discriminator}`` to {member.server.name}**\n\nPlease 🙏 do not forget to respect each others or follow the rules.'
@@ -3939,11 +3939,23 @@ async def on_member_join(member):
            await bot.send_message(channel, embed=embed) 
 
 @bot.event
+async def on_member_join(member):
+    for channel in member.server.channels:
+        if channel.name == '🌟↪welcome-goodbye↩🌟':
+           embed = discord.Embed(color=0xC72323)
+           embed.set_author(name="🎉 New member has joined 🎉")
+           embed.description = f'**Welcome ``{member.name}#{member.discriminator}`` to {member.server.name}**\n\nPlease 🙏 do not forget to respect each others or follow the rules.'
+           embed.set_thumbnail(url=member.avatar_url)
+           embed.timestamp = datetime.datetime.utcnow()
+           embed.set_footer(text='We are now have {} members'.format(str(member.server.member_count)))
+           await bot.send_message(channel, embed=embed) 
+
+
+@bot.event
 async def on_member_remove(member):
     for channel in member.server.channels:
-        if channel.name == '🌟-welcome-goodbye-🌟':
-            embed = discord.Embed(description=f'Peace out ``{member.name}#{member.discriminator}``✌! We will gonna miss you in {member.server.name} server.', color=0xC72323)
-            embed.set_author(name="👋 Member has left 👋")
+        if channel.name == '🌟-goodbye-🌟':
+            embed = discord.Embed(title="👋 Member has left 👋", description=f'Peace out ``{member.name}#{member.discriminator}``✌!\n\nWe will gonna miss you in the ``{member.server.name}`` server.', color=0xC72323)
             embed.set_thumbnail(url=member.avatar_url)
             embed.timestamp = datetime.datetime.utcnow()
             await bot.send_message(channel, embed=embed)
@@ -3954,7 +3966,8 @@ async def on_member_remove(member):
     for channel in member.server.channels:
         if channel.name == 'welcome':
             embed = discord.Embed(color=0xC72323)
-            embed.description = f'Peace out ``{member.name}#{member.discriminator}``✌! We will gonna miss you in {member.server.name} server.'
+            embed.description = f'Peace out ``{member.name}#{member.discriminator}``✌!\n\nWe will gonna miss you in the ``{member.server.name}`` server.'
+            embed.set_author(name="👋 Member has left 👋")
             embed.set_thumbnail(url=member.avatar_url)
             embed.timestamp = datetime.datetime.utcnow()
             await bot.send_message(channel, embed=embed)
@@ -4571,7 +4584,8 @@ async def welcomer(ctx):
       server = ctx.message.server
       everyone_perms = discord.PermissionOverwrite(send_messages=False, read_messages=True)
       everyone = discord.ChannelPermissions(target=server.default_role, overwrite=everyone_perms)
-      await bot.create_channel(server, '🌟-welcome-goodbye-🌟',everyone)
+      await bot.create_channel(server, '🌟-welcome-🌟',everyone)
+      await bot.create_channel(server, '🌟-goodbye-🌟',everyone)
       await bot.say("I successfully created the welcomer.")
 
 bot.run(os.environ['Token1'])
