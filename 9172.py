072ee6fa7b5c9942ee75cb91767ebf6366f4508c
Love radio v616 <45826030+loveradio1337@users.noreply.github.com>
2021-01-04 22:51:45 +0800

    Update 9172.py

--- 9172.py
+++ 9172.py
@@ -4531,4 +4531,4 @@ async def welcomer(ctx):
       await bot.create_channel(server, '🌟-goodbye-🌟',everyone)
       await bot.say("I successfully created the welcomer.")
 
-bot.run(os.environ['Token123123'])
+bot.run(os.environ['token123123'])
